7ef4d5182afc18d9fd7f7d70572cfae06da5f88d
barooahn <barooahn@gmail.com>
2025-08-02 17:59:54 +0100

    updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-	const baseUrl = "https://sambal-shop.vercel.app";
+	const baseUrl = "https://spiceislandindonesia.com";
 
 	return [
 		{
